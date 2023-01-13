21ee4c91caf00d5f801f6c5985ee978613ce6c79
mauroapjr <mauroapjr@outlook.com>
2023-01-12 22:03:48 -0500

    clean the code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,19 @@
+// TEST
+
+// const eqArrays = function (arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+
+/////////////////////////////////////////////////////////////////////////////
+
 const assertArraysEqual = function (arr1, arr2) {
   if (eqArrays(arr1, arr2) === false) {
     console.log("Assertation failed");
@@ -7,3 +23,20 @@ const assertArraysEqual = function (arr1, arr2) {
 };
 
 console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+
+/////////////////////////////////////////////////////////////////////////////
+
+//TEST
+
+// const words = ["ground", "control", "to", "major", "tom"];
+
+// const map = function (array, callback) {
+//   const results = [];
+//   for (let item of array) {
+//     results.push(callback(item));
+//   }
+//   return results;
+// };
+
+// const results1 = map(words, (word) => word[0]);
+// console.log(results1);
